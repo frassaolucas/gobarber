ccc0ef7dfa79bc957b085f9f55bfe6a106202ed1
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-29 16:08:16 -0300

    isolated components

--- new/frontend/src/pages/SignIn/styles.ts
+++ new/frontend/src/pages/SignIn/styles.ts
@@ -28,71 +28,37 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #F4EDE8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #FF9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 16px;
-      color: #312E38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#FF9000')}
-      }
-    }
-
     a {
-      color: #F4EDE8;
+      color: #f4ede8;
       display: block;
       margin-top: 24px;
       text-decoration: none;
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, '#F4EDE8')}
+        color: ${shade(0.2, '#F4EDE8')};
       }
     }
   }
 
   > a {
-      color: #FF9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
 
-      display: flex;
-      align-items: center;
+    display: flex;
+    align-items: center;
 
-      &:hover {
-        color: ${shade(0.2, '#FF9000')}
-      }
-
-      svg {
-        margin-right: 16px;
-      }
+    &:hover {
+      color: ${shade(0.2, '#FF9000')};
     }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
 `;
 
 export const Background = styled.div`
