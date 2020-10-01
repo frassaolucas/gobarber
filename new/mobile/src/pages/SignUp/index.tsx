1739cd12bf0d649ed7df509ea5587b3a5321e7a6
Lucas Frassao <frassao.lucas@gmail.com>
2020-10-01 07:04:03 -0300

    connected to the api to register users

--- new/mobile/src/pages/SignUp/index.tsx
+++ new/mobile/src/pages/SignUp/index.tsx
@@ -11,6 +11,7 @@ import {
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
@@ -53,9 +54,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
